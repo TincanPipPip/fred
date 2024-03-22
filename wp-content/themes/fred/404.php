ffd4a6beb9c76224ec0f1fc628c5bf1d3c795870
Angelou <angelou.jeffers@un.titled.co.uk>
2024-03-22 16:49:10 +0000

    [setup][404] - build and setup 404 page template and option page control

--- wp-content/themes/fred/404.php
+++ wp-content/themes/fred/404.php
@@ -1,6 +1,8 @@
 <?php
 /**
  * The template for displaying 404 pages (Not Found)
+ * 
+ * Template Name:404 Page
  *
  * Methods for TimberHelper can be found in the /functions sub-directory
  *
@@ -10,21 +12,20 @@
  */
 
 $context = Timber::context();
-$timber_post     = new Timber\Post();
-
+$post_id = get_field('fourohfour_select_404_page', 'options');
+$timber_post = get_post($post_id);
+setup_postdata($timber_post);
 $context['post'] = $timber_post;
-$context['subnav'] = Timber::get_widgets( 'subnav' );
 
 if ( has_blocks( $timber_post->post_content ) ) {
-$blocks = parse_blocks( $post->post_content );
-$contentBlocks = [];
+    $blocks = parse_blocks( $timber_post->post_content );
+    $contentBlocks = [];
+    foreach ($blocks as $block) {
+        $contentBlocks[] = render_block($block);
+    }
 
-foreach ($blocks as $block) {
-    $contentBlocks[] = render_block($block);
-}
-
-// Output the rest of the content instead of using `post.content`
-$context['blocks'] = $contentBlocks;
+    // Output the rest of the content instead of using `post.content`
+    $context['blocks'] = $contentBlocks;
 }
 
 Timber::render( 'pages/404.twig', $context );
