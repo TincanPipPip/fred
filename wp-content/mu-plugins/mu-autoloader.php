0d7203b58f016ac23180d3e701abd6d6be631c50
Angelou <angelou.jeffers@un.titled.co.uk>
2024-04-04 10:57:41 +0100

    [setup][pantheon-bugfix] - remove pantheon.php from mu-plugins - include pantheon-mu-plugin reference within mu-autoloader

--- wp-content/mu-plugins/mu-autoloader.php
+++ wp-content/mu-plugins/mu-autoloader.php
@@ -8,3 +8,4 @@ require WPMU_PLUGIN_DIR.'/advanced-custom-fields-pro/acf.php';
 require WPMU_PLUGIN_DIR.'/timber-library/timber.php';
 require WPMU_PLUGIN_DIR.'/restrict-blocks/index.php';
 require WPMU_PLUGIN_DIR.'/rest-api-blocks/wp-rest-blocks.php';
+require WPMU_PLUGIN_DIR.'/pantheon-mu-plugin/pantheon.php';
