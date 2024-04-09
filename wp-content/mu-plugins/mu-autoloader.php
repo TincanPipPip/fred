2f1788639ffb113287637231ecb4a69a0d5bf813
Angelou <angelou.jeffers@un.titled.co.uk>
2024-04-09 14:03:08 +0100

    [setup] - install and activate wps-hide-plugin within mu-plugin directory

--- wp-content/mu-plugins/mu-autoloader.php
+++ wp-content/mu-plugins/mu-autoloader.php
@@ -9,3 +9,4 @@ require WPMU_PLUGIN_DIR.'/timber-library/timber.php';
 require WPMU_PLUGIN_DIR.'/restrict-blocks/index.php';
 require WPMU_PLUGIN_DIR.'/rest-api-blocks/wp-rest-blocks.php';
 require WPMU_PLUGIN_DIR.'/pantheon-mu-plugin/pantheon.php';
+require WPMU_PLUGIN_DIR.'/wps-hide-login/wps-hide-login.php';
