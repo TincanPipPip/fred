ea0be3175b1a3eff387b268953667dd802c91341
Angelou <angelou.jeffers@un.titled.co.uk>
2024-04-18 10:50:06 +0100

    [setup] - render blocks for homepage and landing page

--- wp-content/themes/fred/template-landing-page.php
+++ wp-content/themes/fred/template-landing-page.php
@@ -8,4 +8,16 @@ $context = Timber::context();
 $timber_post     = new Timber\Post();
 $context['post'] = $timber_post;
 
+if ( has_blocks( $timber_post->post_content ) ) {
+    $blocks = parse_blocks( $post->post_content );
+    $contentBlocks = [];
+  
+    foreach ($blocks as $block) {
+      $contentBlocks[] = render_block($block);
+    }
+    
+    // Output the rest of the content instead of using `post.content`
+    $context['blocks'] = $contentBlocks;
+}
+
 Timber::render( array( 'pages/page-landing.twig' ), $context );
